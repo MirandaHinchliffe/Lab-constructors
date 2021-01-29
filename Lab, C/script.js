0a0c2fc0d3e6c076f051a9c52dc8cc462a206d28
Miranda Hinchliffe <70153183+MirandaHinchliffe@users.noreply.github.com>
2021-01-29 10:53:49 -0500

    Add files via upload

---
+++ Lab, C/script.js
@@ -0,0 +1,12 @@
+function Dog(){
+this.name = "Sparky";
+this.color = "Black"
+this.numLegs = 4;
+}
+this.updatenumLegs = function(){
+    return ++this.numLegs;
+        };
+ let Dog01 = new Dog01("Sparky", "Black", 4)
+    document.getElementById("soso");
+ console.log(dog01.updatenumLegs());
+
